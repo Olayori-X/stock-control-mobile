874ba1545f979c8893a83b2978209f41b1db89f6
Olayori Latubosun <latubosun4328@gmail.com>
2026-09-15 15:39:50 +0100

    outlets

--- lib/features/outlets/presentation/routes.dart
+++ lib/features/outlets/presentation/routes.dart
@@ -2,11 +2,21 @@ import 'package:go_router/go_router.dart';
 import 'package:stock_control_app/core/extensions/string.dart';
 import 'package:stock_control_app/core/navigation/pages.dart';
 import 'package:stock_control_app/features/outlets/presentation/pages/outlet_detail_page.dart';
-
+import 'package:stock_control_app/features/route/domain/repositories/get_route_plan_repository.dart';
 
 final List<GoRoute> outletFeatureRoutes = [
   GoRoute(
     path: Pages.outletDetail.path,
-    builder: (context, state) => const OutletDetailPage(stop: RoutePlanStop, routeDay: '',),
+    builder: (context, state) {
+      final args = state.extra as OutletDetailArgs;
+      return OutletDetailPage(stop: args.stop, routeDay: args.routeDay);
+    },
   ),
 ];
+
+class OutletDetailArgs {
+  final RoutePlanStop stop;
+  final String routeDay;
+
+  const OutletDetailArgs({required this.stop, required this.routeDay});
+}
